afcf8df066f8da7c2d492889538de89d7ebde373
Gabriela Biswasová <pracho.dayat@seznam.cz>
2023-11-27 14:59:40 +0100

    contact added

--- src/pages/contacts/index.jsx
+++ src/pages/contacts/index.jsx
@@ -1,1 +1,10 @@
-export const ContactsPage = () => <div>Contacts</div>
+export const ContactsPage = () => {
+  return (
+    <>
+      <div>Contacts</div>
+      <p>Jitka Poláková</p>
+      <p>IČ: 734100713</p>
+      <p>e-mail:jitkapfau@email.cz</p>
+    </>
+  );
+};
