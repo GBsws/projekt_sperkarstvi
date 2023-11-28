07e7756afba4a80aa47789d3d352705fd28dffa9
Gabriela Biswasová <pracho.dayat@seznam.cz>
2023-11-28 20:44:49 +0100

    workshop addedtext

--- src/pages/workshop/index.jsx
+++ src/pages/workshop/index.jsx
@@ -1,1 +1,16 @@
-export const WorkshopPage = () => <div>Workshop</div>
+import "./style.css";
+export const WorkshopPage = () => {
+  return (
+    <>
+      <div>Moje šperkařská dílna</div>
+      <img src="" alt="" />
+      <img src="" alt="" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A illo magnam
+        nulla corrupti voluptatem eum excepturi explicabo, nesciunt odit in
+        eaque architecto, asperiores ipsam id recusandae ullam repudiandae
+        consequuntur harum!
+      </p>
+    </>
+  );
+};
