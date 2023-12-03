fbd11724ec1926b215efbec07f3d6329339d3db3
Gabriela Biswasová <pracho.dayat@seznam.cz>
2023-12-03 12:26:25 +0100

    homepage styling

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -30,26 +30,26 @@ export const Footer = () => {
 		<div className='footer'>
 			<nav className='footer__navbar'>
 				<div>
-					<NavLink className='footer__link1' to='/vlastnosti-materialu'>
+					<NavLink className='footer__link footer__link1' to='/vlastnosti-materialu'>
 						Vlastnosti materiálu
 					</NavLink>
-					<NavLink className='footer__link1' to='/prodejni-akce'>
+					<NavLink className=' footer__link footer__link1' to='/prodejni-akce'>
 						Prodejní akce a trhy
 					</NavLink>
 				</div>
 				<div>
-					<NavLink className='footer__link2' to='/objednani-a-dodani'>
+					<NavLink className='footer__link footer__link2' to='/objednani-a-dodani'>
 						Objednání a dodávka zboží
 					</NavLink>
-					<NavLink className='footer__link2' to='/dilna'>
+					<NavLink className='footer__link footer__link2' to='/dilna'>
 						Šperkařská dílna
 					</NavLink>
 				</div>
 				<div>
-					<NavLink className='footer__link3' to='/kontakty'>
+					<NavLink className='footer__link footer__link3' to='/kontakty'>
 						Kontaktní údaje
 					</NavLink>
-					<NavLink className='footer__link3' to='/kontakty'>
+					<NavLink className='footer__link footer__link3' to='/kontakty'>
 						Kontaktní údaje
 					</NavLink>
 				</div>
